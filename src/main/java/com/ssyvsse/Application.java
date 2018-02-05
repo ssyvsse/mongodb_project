b947d3e13f0146af9fa21317dcd2eb52efff4e6d
ssyvsse <515124984@qq.com>
2018-02-05 20:58:38 +0800

    tomcat

--- src/main/java/com/ssyvsse/Application.java
+++ src/main/java/com/ssyvsse/Application.java
@@ -2,6 +2,8 @@ package com.ssyvsse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +12,14 @@ import org.springframework.context.annotation.Configuration;
  *
  * @Date 2018年2月3日
  */
-@SpringBootApplication(/*
-						 * exclude = { DataSourceAutoConfiguration.class,
-						 * HibernateJpaAutoConfiguration.class,
-						 * JpaRepositoriesAutoConfiguration.class }
-						 */)
+@SpringBootApplication
 @Configuration
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static ApplicationContext context;
 
